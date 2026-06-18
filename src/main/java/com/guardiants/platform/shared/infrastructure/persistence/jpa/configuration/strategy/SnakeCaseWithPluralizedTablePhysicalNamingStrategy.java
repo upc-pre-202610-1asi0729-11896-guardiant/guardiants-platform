d3009344bf7edc6633058b2817f564379f0a9ec8
genixmvp <genix.mvp@gmail.com>
2026-06-18 03:16:13 -0500

    feat(shared): add infrastructure layer.

---
+++ src/main/java/com/guardiants/platform/shared/infrastructure/persistence/jpa/configuration/strategy/SnakeCaseWithPluralizedTablePhysicalNamingStrategy.java
@@ -0,0 +1,64 @@
+package com.guardiants.platform.shared.infrastructure.persistence.jpa.configuration.strategy;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+import static io.github.encryptorcode.pluralize.Pluralize.pluralize;
+
+/**
+ * PhysicalNamingStrategy that converts entity class names to snake_case
+ * and table names to pluralized snake_case.
+ *
+ * <p>Examples: TelemetryPoint → telemetry_points | VehicleLoan → vehicle_loans</p>
+ *
+ * @since 1.0.0
+ */
+public class SnakeCaseWithPluralizedTablePhysicalNamingStrategy implements PhysicalNamingStrategy {
+
+    @Override
+    public Identifier toPhysicalCatalogName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+
+    @Override
+    public Identifier toPhysicalSchemaName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return this.toSnakeCase(identifier);
+    }
+
+    @Override
+    public Identifier toPhysicalTableName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return this.toSnakeCase(this.toPlural(identifier));
+    }
+
+    @Override
+    public Identifier toPhysicalSequenceName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return this.toSnakeCase(identifier);
+    }
+
+    @Override
+    public Identifier toPhysicalColumnName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return this.toSnakeCase(identifier);
+    }
+
+    /**
+     * Converts an identifier to snake_case.
+     */
+    private Identifier toSnakeCase(final Identifier identifier) {
+        if (identifier == null) return null;
+        final String regex = "([a-z])([A-Z])";
+        final String replacement = "$1_$2";
+        final String newName = identifier.getText()
+                .replaceAll(regex, replacement)
+                .toLowerCase();
+        return Identifier.toIdentifier(newName);
+    }
+
+    /**
+     * Pluralizes an identifier (e.g. TelemetryPoint → TelemetryPoints).
+     */
+    private Identifier toPlural(final Identifier identifier) {
+        final String newName = pluralize(identifier.getText());
+        return Identifier.toIdentifier(newName);
+    }
+}
