4480f77269c39d634da2b4b8e8a9b2713e29b070
genixmvp <genix.mvp@gmail.com>
2026-06-18 18:19:29 -0500

    feat(iam): add account, user and session persistence repositories.

--- src/main/java/com/guardiants/platform/iam/infrastructure/persistence/jpa/repositories/SessionPersistenceRepository.java
+++ src/main/java/com/guardiants/platform/iam/infrastructure/persistence/jpa/repositories/SessionPersistenceRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+// SessionPersistenceRepository.java
 @Repository
 public interface SessionPersistenceRepository
         extends JpaRepository<SessionPersistenceEntity, Long> {
