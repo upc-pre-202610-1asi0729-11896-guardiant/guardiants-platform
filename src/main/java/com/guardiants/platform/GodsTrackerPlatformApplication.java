13dcb8cc56e98ea0d663ad59cd61dde78ed4e4fc
genixmvp <genix.mvp@gmail.com>
2026-06-18 03:06:45 -0500

    chore: update god tracker platform application file.

--- src/main/java/com/guardiants/platform/GodsTrackerPlatformApplication.java
+++ src/main/java/com/guardiants/platform/GodsTrackerPlatformApplication.java
@@ -2,7 +2,13 @@ package com.guardiants.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+/**
+ * Bootstrap class for the GOD's Tracker Platform application.
+ *
+ * <p>Initializes Spring Boot auto-configuration and JPA auditing infrastructure.</p>
+ */
+@EnableJpaAuditing
 @SpringBootApplication
 public class GodsTrackerPlatformApplication {
 
