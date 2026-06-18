06f82fa73ec14b36e8d0e3395111c70a3df80474
genixmvp <genix.mvp@gmail.com>
2026-06-18 18:15:04 -0500

    feat(iam): add account command failure and account command service contract.

--- src/main/java/com/guardiants/platform/iam/application/commandservices/AccountCommandService.java
+++ src/main/java/com/guardiants/platform/iam/application/commandservices/AccountCommandService.java
@@ -1,5 +1,6 @@
 package com.guardiants.platform.iam.application.commandservices;
 
+import com.guardiants.platform.iam.domain.model.aggregates.Account;
 import com.guardiants.platform.iam.domain.model.commands.RegisterAccountCommand;
 import com.guardiants.platform.shared.application.result.Result;
 
