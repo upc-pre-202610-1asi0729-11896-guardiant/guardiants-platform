9077cf63f5f1135e1220883b1f003ea69420a75d
genixmvp <genix.mvp@gmail.com>
2026-06-18 03:03:40 -0500

    chore: initial commit.

---
+++ src/main/java/com/guardiants/platform/GodsTrackerPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.guardiants.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodsTrackerPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GodsTrackerPlatformApplication.class, args);
+    }
+
+}
