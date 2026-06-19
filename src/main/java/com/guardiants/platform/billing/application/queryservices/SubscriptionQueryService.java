babc528dab4d81498187cd8c3511380ffda6c24c
genixmvp <genix.mvp@gmail.com>
2026-06-19 17:11:12 -0500

    feat(billing): add stripe webhook service.

--- src/main/java/com/guardiants/platform/billing/application/queryservices/SubscriptionQueryService.java
+++ src/main/java/com/guardiants/platform/billing/application/queryservices/SubscriptionQueryService.java
@@ -3,10 +3,12 @@ package com.guardiants.platform.billing.application.queryservices;
 import com.guardiants.platform.billing.domain.model.aggregates.Subscription;
 import com.guardiants.platform.billing.domain.model.queries.GetCurrentSubscriptionQuery;
 import com.guardiants.platform.billing.domain.model.queries.GetSubscriptionByIdQuery;
+import com.guardiants.platform.billing.domain.model.queries.GetSubscriptionByStripeIdQuery;
 
 import java.util.Optional;
 
 public interface SubscriptionQueryService {
     Optional<Subscription> handle(GetCurrentSubscriptionQuery query);
     Optional<Subscription> handle(GetSubscriptionByIdQuery query);
+    Optional<Subscription> handle(GetSubscriptionByStripeIdQuery query);
 }
