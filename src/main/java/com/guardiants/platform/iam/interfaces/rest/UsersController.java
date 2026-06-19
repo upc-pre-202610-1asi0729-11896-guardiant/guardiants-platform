249407537cd82c16410f79000e6031d0909bc0b4
genixmvp <genix.mvp@gmail.com>
2026-06-18 19:19:33 -0500

    feat(iam): add update profile endpoint.

--- src/main/java/com/guardiants/platform/iam/interfaces/rest/UsersController.java
+++ src/main/java/com/guardiants/platform/iam/interfaces/rest/UsersController.java
@@ -1,10 +1,15 @@
 package com.guardiants.platform.iam.interfaces.rest;
 
+import com.guardiants.platform.iam.application.commandservices.UserCommandService;
 import com.guardiants.platform.iam.application.queryservices.UserQueryService;
 import com.guardiants.platform.iam.domain.model.queries.GetUserByIdQuery;
+import com.guardiants.platform.iam.interfaces.rest.resources.ProfileUpdateResource;
+import com.guardiants.platform.iam.interfaces.rest.transform.ResponseEntityFromUserCommandResultAssembler;
 import com.guardiants.platform.iam.interfaces.rest.transform.ResponseEntityFromUserQueryResultAssembler;
+import com.guardiants.platform.iam.interfaces.rest.transform.UpdateProfileCommandFromResourceAssembler;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +27,8 @@ public class UsersController {
     private final MessageSource messageSource;
 
     public UsersController(UserQueryService userQueryService,
-                           MessageSource messageSource) {
+                           MessageSource messageSource,
+                           UserCommandService userCommandService) {
         this.userQueryService = userQueryService;
         this.messageSource = messageSource;
     }
@@ -37,5 +43,17 @@ public class UsersController {
                     messageSource, "iam.error.invalidCredentials", userId);
         }
         return ResponseEntityFromUserQueryResultAssembler.toResponseEntityFromUser(user);
+    }
+
+    @Operation(summary = "Update profile", description = "Updates the user name and/or email.")
+    @PutMapping("/{userId}/profile")
+    public ResponseEntity<?> updateProfile(@PathVariable Long userId,
+                                           @Valid @RequestBody ProfileUpdateResource resource) {
+        log.debug("PUT /api/v1/iam/users/{}/profile", userId);
+        var command = UpdateProfileCommandFromResourceAssembler
+                .toCommandFromResource(userId, resource);
+        var result = userCommandService.handle(command);
+        return ResponseEntityFromUserCommandResultAssembler
+                .toResponseEntityFromResult(result, messageSource);
     }
 }
