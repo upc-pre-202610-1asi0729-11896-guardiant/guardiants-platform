025d42f9fe7204ba5c654140912b28a571894d92
genixmvp <genix.mvp@gmail.com>
2026-06-18 19:04:18 -0500

    feat(iam): add user queries.

---
+++ src/main/java/com/guardiants/platform/iam/domain/model/queries/ExistsUserByIdQuery.java
@@ -0,0 +1,4 @@
+package com.guardiants.platform.iam.domain.model.queries;
+
+public record ExistsUserByIdQuery(Long userId) {
+}
---
+++ src/main/java/com/guardiants/platform/iam/domain/model/queries/GetUserByEmailQuery.java
@@ -0,0 +1,4 @@
+package com.guardiants.platform.iam.domain.model.queries;
+
+public record GetUserByEmailQuery(String email) {
+}
